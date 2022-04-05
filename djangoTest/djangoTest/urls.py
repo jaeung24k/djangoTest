097d343bd482d0295b4eb498c6cdc3a4b2655d3d
jaeung24k <jaeung24k@naver.com>
2022-04-05 10:24:29 +0900

    FE: Add Enter-page, AboutMe-page

--- djangoTest/djangoTest/urls.py
+++ djangoTest/djangoTest/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),    
+    path('', include('single_pages.urls')),
 ]
